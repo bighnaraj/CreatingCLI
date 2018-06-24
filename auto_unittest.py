5b8a6ba777476778050e6ccae898b546e8b3ebb3
bighnaraj <bighnaraj.nit@gmail.com>
2018-06-24 14:41:26 +0530

    adding sample unittest module

---
+++ auto_unittest.py
@@ -0,0 +1,16 @@
+import unittest
+import auto
+ 
+class TestUM(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def get_sources(self):
+        pass
+ 
+    def get_nodes(self):
+        pass
+ 
+if __name__ == '__main__':
+    unittest.main()
